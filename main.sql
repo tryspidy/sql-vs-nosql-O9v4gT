781dfde1379fe86e1ce333ee487c5b8405ed6465
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-19 19:27:23 +0530

    Initial commit

---
+++ main.sql
@@ -0,0 +1,18 @@
+-----------------
+key differences |
+-----------------
+SQL databases are usually RDBMS or DBMS
+(Relational DataBase Management Systems) and are table based,
+NoSQL uses unstructured data (like graphs)
+
+-----------------
+Examples        |
+-----------------
+SQL Databases:
+MySQL
+SQLite
+Oracle DB
+
+NoSQL:
+MongoDB
+DynamoDB
